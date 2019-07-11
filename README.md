892b63737a88d16b481a7967b6bc3c7f6d83b829
Cole Rudolph <crudolph@gmail.com>
2019-07-11 17:18:32 -0400

    only animate/still onclick event todo

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 **TODO:**
 
-1. submit button onclick event to append query buttons
+1. ~submit button onclick event to append query buttons~
 
 2. image onclick event to start/stop animation
--- assets/app.js
+++ assets/app.js
@@ -4,18 +4,17 @@ console.log("( ͡◉ ͜ʖ ͡◉)");
 var gifQueries = [
     "Bears",
     "Beets",
-    // this string console logs as only "Battlestar", why?
-    // TODO:  try as a nested array
-    "Battlestar Galactica"
+    ["Battlestar", "Galactica"],
 ];
 
 // for loop appending query array as buttons
 for (var i = 0; i < gifQueries.length; i++) {
     // why does only ` work as the html wrapper here, why won't single or double quotes work?
-    $("#queryButtons").append(`<button class ="btn-outline-danger" data-attr=${gifQueries[i]}>${gifQueries[i]} </button>`);
+    // why am I restricted to a continuous string without spaces for the button class?  btn btn-outline-danger causes issues with the onclick event
+    $("#queryButtons").append(`<button class= "btn-outline-danger" data-attr= ${gifQueries[i]}>${gifQueries[i]} </button>`);
 };
 
-// why is the class needed for the onclick event here?
+// why is the class needed for the onclick event here?  The event does not register without it.
 $("#queryButtons").on("click", ".btn-outline-danger", function(event) {
     event.preventDefault();
     var queryText = $(this).attr("data-attr");
@@ -29,11 +28,19 @@ $("#queryButtons").on("click", ".btn-outline-danger", function(event) {
         console.log(response);
         var data = response.data;
         for (var i = 0; i < response.data.length; i++) {
-            $("#queryResults").prepend(`<img class="gifimages" data-still="${data[i].images.fixed_height_still.url}"data-animate ="${data[i].images.fixed_height.url}" data-state="animate" src="${data[i].images.fixed_height.url}" width=310 height=310 />`)
+            $("#queryResults").prepend(`<img class= "gifimages" data-still= "${data[i].images.fixed_height_still.url}"data-animate= "${data[i].images.fixed_height.url}" data-state="animate" src="${data[i].images.fixed_height.url}" width=310 height=310 />`)
         }
     });
 });
 
 // submit button onclick event
+$("#submit").on("click", function(event) {
+    event.preventDefault();
+    var query = $("#gifQuery").val().trim();
+    console.log(query);
+    gifQueries.push(query);
+    console.log(gifQueries);
+    $("#queryButtons").append(`<button class= "btn-outline-danger"  data-attr= ${query}>${query}</button>`)
+})
 
 // query results onclick event to start/stop animation
